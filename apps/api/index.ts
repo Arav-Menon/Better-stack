dbf2731cb3b38f184ae44ea3bbb8a3e7adf0d895
Arav menon <aravmenon.ak@gmail.com>
2025-09-08 15:52:47 +0530

    setup site router

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import { siteRouter } from "./src/sites/add_site";
+import { siteRouter } from "./src/sites/site";
 import cookieParser from "cookie-parser";
 import { authRouter } from "./src/auth/user_auth/auth";
 import { remove_user_router } from "./src/auth/user_auth/remove_user";
 import { update_user_router } from "./src/auth/user_auth/update_user";
 import { userRouter } from "./src/auth/user_auth/user";
 import "dotenv/config";
+import { update_site_router } from "./src/sites/edit_site";
+import { remove_site_router } from "./src/sites/remove_site";
+import { add_site_router } from "./src/sites/add_site";
 
 export const app = express();
 app.use(cookieParser());
@@ -16,15 +19,15 @@ app.use(express.json());
 
 // User router
 app.use("/user", authRouter);
-app.use("/user", update_user_router);
 app.use("/user", userRouter);
+app.use("/user", update_user_router);
 app.use("/user", remove_user_router);
 
 // site router
+app.use("/site", add_site_router);
 app.use("/site", siteRouter);
-app.use("/site", siteRouter);
-app.use("/site", siteRouter);
-app.use("/site", siteRouter);
+app.use("/site", update_site_router);
+app.use("/site", remove_site_router);
 
 app.listen(PORT, () => {
   console.log(`Server is started on PORT ${PORT}`);
