f13699b9433b89453756e31dc4535e2610872205
Arav menon <aravmenon.ak@gmail.com>
2025-09-08 15:31:53 +0530

    make some auth schema optional

--- packages/lib/validations.ts
+++ packages/lib/validations.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
 export const authSchema = z.object({
-  fullName: z.string().min(3),
+  fullName: z.string().min(3).optional(),
   email: z.string().email(),
   password: z.string().min(6),
-  confirmPassword: z.string().min(6),
-  companyName: z.string(),
-  otp: z.string().min(4).max(4),
-  phoneNumber: z.string().min(10).max(10),
+  confirmPassword: z.string().min(6).optional(),
+  companyName: z.string().optional(),
+  otp: z.string().min(4).max(4).optional(),
+  phoneNumber: z.string().min(10).max(10).optional(),
 });
 
 export const addSite = z.object({
   site_name: z.string(),
-  url: z.string().startsWith("https://"),
+  url: z.string(),
 });
