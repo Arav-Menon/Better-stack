c3aa1961d388223e758aaf48253499bae0aac99d
Arav menon <aravmenon.ak@gmail.com>
2025-09-09 11:23:45 +0530

    add error handling in db for sites

--- apps/api/src/sites/add_site.ts
+++ apps/api/src/sites/add_site.ts
@@ -5,42 +5,52 @@ import { authMiddleware } from "../../middleware";
 
 export const add_site_router = express.Router();
 
-add_site_router.post("/add-site", authMiddleware, async (req: any, res: any) => {
-  const user_id = req.id;
+add_site_router.post(
+  "/add-site",
+  authMiddleware,
+  async (req: any, res: any) => {
+    const user_id = req.id;
 
-  try {
-    const result = addSite.safeParse(req.body);
+    try {
+      const result = addSite.safeParse(req.body);
 
-    if (!result.success) return res.status(400).json({ error: result.error });
+      if (!result.success) return res.status(400).json({ error: result.error });
 
-    const { site_name, url } = result.data;
-    const existSite = await db.website.findUnique({ where: { id: url } });
+      const { site_name, url } = result.data;
+      const existSite = await db.website.findUnique({ where: { id: url } });
 
-    if (existSite) {
-      return res.status(403).json({
-        message: `website with this name${site_name} already exist, `,
+      if (existSite) {
+        return res.status(403).json({
+          message: `website with this name${site_name} already exist, `,
+        });
+      }
+
+      try {
+        const createSite = await db.website.create({
+          data: {
+            site_name: site_name,
+            url: url,
+            user: {
+              connect: {
+                id: user_id,
+              },
+            },
+          },
+        });
+
+        return res.status(200).json({
+          createSite,
+        });
+      } catch (err) {
+        return res.status.json({
+          error: err,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        message: e,
       });
     }
-
-    const createSite = await db.website.create({
-      data: {
-        site_name: site_name,
-        url: url,
-        user: {
-          connect: {
-            id: user_id,
-          },
-        },
-      },
-    });
-
-    return res.status(200).json({
-      createSite,
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      message: e,
-    });
   }
-});
+);
