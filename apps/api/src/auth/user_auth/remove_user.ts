5652203cc127c88a4c3f9c33d1c6122ffbca6d24
Arav menon <aravmenon.ak@gmail.com>
2025-09-09 11:17:09 +0530

    fix deleting user and webiste

--- apps/api/src/auth/user_auth/remove_user.ts
+++ apps/api/src/auth/user_auth/remove_user.ts
@@ -4,16 +4,27 @@ import { db } from "@repo/db/db";
 
 export const remove_user_router = Router();
 
-remove_user_router.delete("/remove-account", authMiddleware, async (req, res) => {
-  try {
-    const userId = (req as any).user.id;
+remove_user_router.delete(
+  "/remove-account/:userId/:webisteId",
+  authMiddleware,
+  async (req, res) => {
+    try {
+      const userId = (req as any).user.id;
+      const websiteId = req.params.webisteId;
 
-    await db.user.delete({
-      where: { id: userId },
-    });
+      const user = await db.user.delete({
+        where: { id: userId },
+      });
+      const userAddedWebsite = await db.website.delete({
+        where: {
+          user_id: userId,
+          id: websiteId,
+        },
+      });
 
-    res.json({ message: "User deleted successfully" });
-  } catch (err) {
-    res.status(500).json({ error: "Server error" });
+      res.json({ userAddedWebsite, user });
+    } catch (err) {
+      res.status(500).json({ error: "Server error" });
+    }
   }
-});
+);
