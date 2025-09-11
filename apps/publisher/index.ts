b5a907f7cf3219a40ca1cea397d92105298a7ccb
Arav menon <aravmenon.ak@gmail.com>
2025-09-11 09:07:11 +0530

    add redis stream to pick webistes from db

--- apps/publisher/index.ts
+++ apps/publisher/index.ts
@@ -1,2 +1,29 @@
 import { db } from "@repo/db/db";
+import { xAddBulk } from "@repo/redis-streams/redis_streams";
 
+async function main() {
+  let websites = await db.website.findMany({
+    select: {
+      url: true,
+      id: true,
+    },
+  });
+
+  console.log(websites.length);
+
+  await xAddBulk(
+    websites.map((w) => ({
+      url: w.url,
+      id: w.id,
+    }))
+  );
+}
+
+setInterval(
+  () => {
+    main();
+  },
+  3 * 1000 * 60
+);
+
+main();
