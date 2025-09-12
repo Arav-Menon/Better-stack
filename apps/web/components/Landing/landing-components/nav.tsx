a69a06181fb29bf1861f27bccb3ba135c994905a
Arav menon <aravmenon.ak@gmail.com>
2025-09-12 13:18:42 +0530

    add blur background to the navbar

--- apps/web/components/Landing/landing-components/nav.tsx
+++ apps/web/components/Landing/landing-components/nav.tsx
@@ -8,7 +8,7 @@ export function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-50 backdrop-blur-md ">
+    <header className="sticky top-0 z-50 backdrop-blur-md' ">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center space-x-2">
