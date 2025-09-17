2f4b1cb643363e78b4de03751422bc76893f40e9
Arav menon <aravmenon.ak@gmail.com>
2025-09-17 17:42:38 +0530

    Add new ui from shadCn

--- apps/web/components/ui/input.tsx
+++ apps/web/components/ui/input.tsx
@@ -1,42 +1,21 @@
-import { cn } from "@/lib/utils";
+import * as React from "react"
 
-interface CustomInput {
-  type: string;
-  placeholder: string;
-  variant: string;
-  className?: string;
-  autoComplete?: string;
-  required?: boolean;
-  onChange: (e: any) => void;
-}
+import { cn } from "@/lib/utils"
 
-export default function CustomInput({
-  type,
-  placeholder,
-  variant = "default",
-  className,
-  onChange,
-  autoComplete,
-  required,
-}: CustomInput): React.JSX.Element {
-  const variants = {
-    default:
-      "w-full px-4 py-3 border border-gray-700 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white/80 focus:border-transparent transition-all duration-200",
-  };
-
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
-    <>
-      <input
-        type={type}
-        placeholder={placeholder}
-        className={cn(
-          variants[variant as keyof typeof variants] ?? variants.default,
-          className
-        )}
-        onChange={onChange}
-        autoComplete={autoComplete}
-        required={required}
-      />
-    </>
-  );
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        className
+      )}
+      {...props}
+    />
+  )
 }
+
+export { Input }
