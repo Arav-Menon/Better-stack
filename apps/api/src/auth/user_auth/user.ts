883b20b465a17f7d998221b289ff6f0a11c63df6
Arav menon <aravmenon.ak@gmail.com>
2025-09-23 22:59:49 +0530

    git add .

--- apps/api/src/auth/user_auth/user.ts
+++ apps/api/src/auth/user_auth/user.ts
@@ -23,7 +23,9 @@ userRouter.get("/profile", authMiddleware, async (req, res) => {
 
     if (!user) return res.status(404).json({ error: "User not found" });
 
-    res.json(user);
+    res.status(200).json({
+      user: user,
+    });
   } catch (err) {
     res.status(500).json({ error: "Server error" });
   }
